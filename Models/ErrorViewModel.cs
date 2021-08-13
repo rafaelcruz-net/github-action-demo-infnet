e2b3e8950d7badecb7bfee402944c876bcff678b
Rafael Cruz <rafaelcruz.net81@gmail.com>
2021-08-13 20:48:16 -0300

    Criando projeto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace github_action_demo_infnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
