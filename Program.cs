e45699126a133559815f97e0e66fd955a159387a
Rafael Cruz <rafaelcruz.net81@gmail.com>
2021-08-13 21:36:02 -0300

    Ajuste do program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace github_action_demo_infnet
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run()
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
